b62c2f8db077a2194befecf6204f6d267abd73fc
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-01-13 22:19:17 +0300

    added jquery codes

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ function Contact(first, last) {
   alert(Object.values(home))
   jane.address.push(home);
   alert(Object.values(jane))
+
+
+$(document).ready(function() {
+$("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                '<div class="form-group">' +
+                                    '<label for="new-street">Street</label>' +
+                                    '<input type="text" class="form-control new-street">' +
+                                '</div>' +
+                                '<div class="form-group">' +
+                                    '<label for="new-city">City</label>' +
+                                    '<input type="text" class="form-control new-city">' +
+                                '</div>' +
+                                '<div class="form-group">' +
+                                    '<label for="new-county">County</label>' +
+                                    '<input type="text" class="form-control new-county">' +
+                                '</div>' +
+                                '</div>');
+});
+})
